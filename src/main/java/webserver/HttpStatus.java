59ac405d20a05540a61d32a6323d19516b2af40a
ddingmin <ddingminyt@gmail.com>
2024-04-09 17:15:02 +0900

    feat: httpStatus enum 구현

---
+++ src/main/java/webserver/HttpStatus.java
@@ -0,0 +1,32 @@
+package webserver;
+
+import java.util.Arrays;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int value;
+    private final String message;
+
+    HttpStatus(int value, String message) {
+        this.value = value;
+        this.message = message;
+    }
+
+    public static HttpStatus valueOf(int statusCode) {
+        return Arrays.stream(HttpStatus.values())
+                .filter(it -> it.value == statusCode)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("일치 하는 상태 코드가 없습니다."));
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

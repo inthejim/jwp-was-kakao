1ae1252a063da96a6872499e4dbc9ec751061b2f
jenna.me <inthejim24@gmail.com>
2024-04-17 10:15:43 +0900

    refactor: 상수화

--- src/main/java/webserver/http/Headers.java
+++ src/main/java/webserver/http/Headers.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Map;
 
 public class Headers {
+    public static final int KEY_INDEX = 0;
+    public static final int VALUE_INDEX = 1;
+
     private Map<String, String> headers = new HashMap<>();
 
     public Headers(List<String> headers) {
         this.headers.put("Content-Length", "0");
         headers.stream()
                 .map(it -> it.split(":", 2))
-                .forEach(it -> this.headers.put(it[0], it[1].trim()));
+                .forEach(it -> this.headers.put(it[KEY_INDEX], it[VALUE_INDEX].trim()));
     }
 
     public String get(String key) {
